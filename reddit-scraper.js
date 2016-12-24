a09114666522c10fd8656071aad889e3d08ef67e
Dave Gumba <dave_gumba@Yahoo.com>
2016-12-24 15:00:51 -0500

    Figuring out how to call rp with two parameters

--- reddit-scraper.js
+++ reddit-scraper.js
@@ -1,6 +1,9 @@
-// Scrapes r/theonion and r/nottheonion post titles,
-// Puts them in a json file.
-// http://stackoverflow.com/questions/9036429/convert-object-string-to-json
+/*
+Scrapes r/theonion and r/nottheonion for post titles.
+Writes those titles in JSON.
+
+TODO: Find a way to make the sub distinct.
+*/
 
 var cheerio = require('cheerio');
 var rp = require('request-promise');
@@ -8,16 +11,20 @@ var fsp = require('fs-promise');
 
 var onion_url = "https://www.reddit.com/r/theonion";
 var not_onion_url = "https://www.reddit.com/r/nottheonion";
+var posts = [];
 
 function parse(html) {
-    var result = '';
     var $ = cheerio.load(html);
+
     $("div#siteTable > div.link").each(function(idx) {
         var title = $(this).find('p.title > a.title').text().trim();
-        console.log(title);
-        result += title + '\n';
+        var post_object = '{ "sub" : "theonion", "title" : "' + title + '"}';
+        console.log(post_object);
+        posts.push(post_object);
     });
-    return result;
+
+    var posts_as_json = JSON.stringify(posts);
+    return posts_as_json;
 }
 
 // what this do?
@@ -25,12 +32,12 @@ var append = file => content => fsp.appendFile(file, content);
 
 rp(onion_url)
     .then(parse)
-    .then(append('onion.txt'))
+    .then(append('onion.json'))
     .then(() => console.log('Onion Success'))
     .catch(err => console.log('Error: ', err));
 
 rp(not_onion_url)
     .then(parse)
-    .then(append('not_onion.txt'))
+    .then(append('not_onion.json'))
     .then(() => console.log('Not Onion Success'))
     .catch(err => console.log('Error: ', err));
