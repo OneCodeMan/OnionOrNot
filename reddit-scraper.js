9ead09116647dabce7493fcc293ef6817bc019ab
Dave Gumba <dave_gumba@Yahoo.com>
2016-12-24 13:49:50 -0500

    Scrape is working

--- reddit-scraper.js
+++ reddit-scraper.js
@@ -1,6 +1,36 @@
 // Scrapes r/theonion and r/nottheonion post titles,
-// Puts them in a json file
+// Puts them in a json file.
+// http://stackoverflow.com/questions/9036429/convert-object-string-to-json
 
-var request = require('request');
 var cheerio = require('cheerio');
-var fs = require('fs');
+var rp = require('request-promise');
+var fsp = require('fs-promise');
+
+var onion_url = "https://www.reddit.com/r/theonion";
+var not_onion_url = "https://www.reddit.com/r/nottheonion";
+
+function parse(html) {
+    var result = '';
+    var $ = cheerio.load(html);
+    $("div#siteTable > div.link").each(function(idx) {
+        var title = $(this).find('p.title > a.title').text().trim();
+        console.log(title);
+        result += title + '\n';
+    });
+    return result;
+}
+
+// what this do?
+var append = file => content => fsp.appendFile(file, content);
+
+rp(onion_url)
+    .then(parse)
+    .then(append('onion.txt'))
+    .then(() => console.log('Onion Success'))
+    .catch(err => console.log('Error: ', err));
+
+rp(not_onion_url)
+    .then(parse)
+    .then(append('not_onion.txt'))
+    .then(() => console.log('Not Onion Success'))
+    .catch(err => console.log('Error: ', err));
