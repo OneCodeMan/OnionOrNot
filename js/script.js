f3d73e6722f91aec6b055a76badcdd397910fcf2
Dave Gumba <dave_gumba@Yahoo.com>
2016-12-26 13:14:34 -0500

    I'm still working on ajax

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-// https://github.com/OneCodeMan/OnionOrNot/blob/master/onionornotdata.json
+/*
+TODO: Make the GET fetch as clean as possible.
+TODO: Check that you can identify each post by their "sub" property
+TODO: Randomly generate a post
+TODO: Display post on web page
+TODO: Find a way to avoid duplication of posts? Like posts occuring again.
+TODO: 
+*/
+
+
 var request = new XMLHttpRequest();
-request.open('GET', 'https://github.com/OneCodeMan/OnionOrNot/blob/master/onionornotdata.json');
+var json_url = "https://api.myjson.com/bins/s474n";
+request.open('GET', json_url);
+
+request.onload = function() {
+    var json_data = JSON.parse(request.responseText);
+};
+
+request.send();
