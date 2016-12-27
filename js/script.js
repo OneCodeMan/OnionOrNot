073a412605aa6e448edd6df0b34a191ad08e1495
Dave Gumba <dave_gumba@Yahoo.com>
2016-12-26 19:39:22 -0500

    Changed function names for readability.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function generateRandNum(min, max) {
     return Math.floor(Math.random() * (maximum - minimum)) + minimum;
 }
 
-function generateNew(json_data) {
+function generateNewPost(json_data) {
     var rand_num = generateRandNum(0, json_data.length);
     console.log("generateNew length: ", json_data.length);
     var rand_post = json_data[rand_num];
@@ -17,33 +17,33 @@ function generateNew(json_data) {
 var post_title = document.getElementById("post-title");
 var onion_button = document.getElementById("onion-chosen");
 var not_onion_button = document.getElementById("not-onion-chosen");
+var score_display = document.getElementById("score");
 
 var req = new XMLHttpRequest();
 var json_url = "https://api.myjson.com/bins/s474n";
 var score = 0;
-
-console.log("Score: ", score);
 
 req.open('GET', json_url);
 req.onload = function() {
 
     if (req.status === 200) {
         var json_data = JSON.parse(req.responseText);
-        var rand_post = generateNew(json_data);
+        var rand_post = generateNewPost(json_data);
 
         post_title.innerHTML = rand_post.content;
+        score_display.innerHTML = score;
 
         onion_button.onclick = function() {
             if (rand_post.sub === "theonion") {
                 score++;
-                console.log("Score: ", score);
+                score_display.innerHTML = score;
             } else {
                 score--;
-                console.log("Score: ", score);
+                score_display.innerHTML = score;
             }
 
             // update title
-            rand_post = generateNew(json_data);
+            rand_post = generateNewPost(json_data);
             var index = json_data.indexOf(rand_post);
 
 
@@ -60,14 +60,14 @@ req.onload = function() {
         not_onion_button.onclick = function() {
             if (rand_post.sub === "nottheonion") {
                 score++;
-                console.log("Score: ", score);
+                score_display.innerHTML = score;
             } else {
                 score--;
-                console.log("Score: ", score);
+                score_display.innerHTML = score;
             }
 
             // update title
-            rand_post = generateNew(json_data);
+            rand_post = generateNewPost(json_data);
             var index = json_data.indexOf(rand_post);
 
 
@@ -83,4 +83,5 @@ req.onload = function() {
         console.log("Error");
     }
 };
+
 req.send();
