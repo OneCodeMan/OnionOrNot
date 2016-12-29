ba2168c02a5cf458c58dbb2bec7c343b6c3b6f98
Dave Gumba <dave_gumba@Yahoo.com>
2016-12-28 19:41:19 -0500

    Pondering about how to make the UI/UX better.

--- js/script.js
+++ js/script.js
@@ -15,18 +15,14 @@ var score_display = document.getElementById("score");
 var req = new XMLHttpRequest();
 var json_url = "https://api.myjson.com/bins/1505d7";
 var score = 0;
-var index = -1;
+var index = 0;
 var options = ["theonion", "nottheonion"];
 
 req.open('GET', json_url);
 req.onload = function() {
 
     if (req.status === 200) {
-        json_raw_data = JSON.parse(req.responseText);
-        json_data = Array.from(new Set(json_raw_data));
-
-        console.log(json_raw_data.length);
-        console.log(json_data.length);
+        json_data = JSON.parse(req.responseText);
 
         onion_button.onclick = () => update(0);
         not_onion_button.onclick = () => update(1);
@@ -41,16 +37,20 @@ req.send();
 
 // updates score, updates title, and removes already used indices
 var update = function(user_guess) {
-    if (user_guess != null) {
-        if (options[user_guess] == json_data[index].sub) {
-            score++;
-        } else {
-            score--;
+    if (json_data.length > 0) {
+        if (user_guess != null) {
+            if (options[user_guess] == json_data[index].sub) {
+                score++;
+            } else {
+                score--;
+            }
+            score_display.innerHTML = score;
         }
+        index = Math.floor(Math.random() * (json_data.length - 1));
+        post_title.innerHTML = json_data[index].content; // this is what i'd change to scale size
+        json_data.splice(index, 1);
         score_display.innerHTML = score;
+    } else {
+        post_title.innerHTML = "Thanks for playing. Score: " + score;
     }
-    index = Math.floor(Math.random() * (json_data.length + 1));
-    post_title.innerHTML = json_data[index].content; // this is what i'd change to scale size
-    json_data.splice(index, 1);
-    score_display.innerHTML = score;
 }
